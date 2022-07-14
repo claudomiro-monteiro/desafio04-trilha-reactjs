0576bc88c68d3b7015fbbab91df133d8dfcc1497
claudomiro-monteiro <claudomiromonteiro@gmail.com>
2022-07-14 17:19:55 -0300

    challenge concluded

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,43 @@
+import { FiPlusSquare } from 'react-icons/fi';
+
+import { Container } from './styles';
+import Logo from '../../assets/logo.svg';
+
+interface PropsModal {
+  openModal: () => void;
+}
+
+export function Header({ openModal }: PropsModal) {
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button
+              type="button"
+              onClick={openModal}
+            >
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  )
+}
+
+// class Header extends Component {
+//   render() {
+//     const { openModal } = this.props;
+
+// return (
+      
+//     )
+//   }
+// };
+
+// export default Header;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { Switch, Route } from 'react-router-dom';
 
-import Dashboard from '../pages/Dashboard';
+import { Dashboard } from '../pages/Dashboard';
 
-const Routes = () => (
+const Routes = () => ( 
   <Switch>
     <Route path="/" exact component={Dashboard} />
   </Switch>
